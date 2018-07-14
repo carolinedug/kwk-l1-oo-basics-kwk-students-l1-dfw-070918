4479b3b4abab0079ef27390813741f8192aebb78
caroline dugan <caroline.dugan@gmail.com>
2018-07-14 04:06:09 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -17,10 +17,11 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
-
   def initialize(title)
     @title = title
   end
+  def title
+    @title
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
